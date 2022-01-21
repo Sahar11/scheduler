6e1f39bb5a594102feae96707c8276e0afaa5db7
Sahar11 <saharhalim151@gmail.com>
2022-01-20 21:37:43 -0500

    add & edit Show, Confirm, Empty and Status components

--- stories/index.js
+++ stories/index.js
@@ -5,9 +5,13 @@ import { action } from "@storybook/addon-actions";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+// Appointment
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 import "index.scss";
 
@@ -141,3 +145,24 @@ storiesOf("Button", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+    <Show 
+      student="Lydia Miller-Jones" 
+      interviewer={interviewer} 
+      onEdit={action("onEdit")} 
+      onDelete={action("onDelete")} 
+      />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+    message = "Delete the appointment?"
+    onConfirm={action("onCOnfirm")}
+    onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", ()=> 
+  <Status 
+  message="Deleting"
+  />
+  )
+  
